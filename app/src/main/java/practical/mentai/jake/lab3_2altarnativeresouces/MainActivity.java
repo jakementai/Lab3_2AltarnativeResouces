f1c5474714f29b7eade13ad74a313e41e7315246
Jake <yeemh-wa16@student.tarc.edu.my>
2018-11-08 11:59:38 +0800

    Hello World

---
+++ app/src/main/java/practical/mentai/jake/lab3_2altarnativeresouces/MainActivity.java
@@ -0,0 +1,24 @@
+package practical.mentai.jake.lab3_2altarnativeresouces;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+    }
+
+    public void showMessage(View view){
+    textView.setText(getString(R.string.textViewMessage));
+    }
+}
